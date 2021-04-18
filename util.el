9f3f9778146dfe5f7ad1c646d0d8cbf1f18bc22b
calvin <weng.kai@foxmail.com>
2021-04-18 11:09:35 +0800

    Update util.el

--- util.el
+++ util.el
@@ -1,7 +1,9 @@
-(defmacro wk.eval-return (body)
+(defmacro wk.eval-return (&rest body)
+
   `(catch '37_04_18_101335
-     ,body))
+     (progn ,@body)))
 (defmacro wk.return (&optional args)
   `(throw '37_04_18_101335 ,args))
 ;; (wk.eval-return (wk.return))
 ;; (wk.eval-return (if t (wk.return '("a" 1))))
+;; (wk.eval-return (if nil (wk.return '("a" 1))) (message "37_04_18_104329"))
